3fade97149d437cb21f85d2b5d9eaa0d2ea9d741
Džuris <juris@glaive.pro>
2021-05-08 20:15:48 +0300

    Improve testing

--- bootstrapForTesting.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$codeDir = __DIR__ . DIRECTORY_SEPARATOR .'src';
-
-require_once $codeDir. DIRECTORY_SEPARATOR .'OptionsTrait.php';
-
-foreach (scandir($codeDir) as $file)
-	if (strpos($file, '.php'))
-		require_once $codeDir. DIRECTORY_SEPARATOR .$file;
