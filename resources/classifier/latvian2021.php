6ab37387a99a2f0a9db3c1e5d525a12517a2e676
Džuris <juris@glaive.pro>
2021-05-09 02:24:05 +0300

    Stub latvian2021 classification

---
+++ resources/classifier/latvian2021.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @param array $constants
+ *   Array containing coefficients to calculate points.
+ *
+ *   @type float $constants['edition']['gender']['discipline']['resultShift']
+ *   @type float $constants['edition']['gender']['discipline']['conversionFactor']
+ *   @type float $constants['edition']['gender']['discipline']['exponent']
+ */
+return [];
