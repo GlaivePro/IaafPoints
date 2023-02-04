4678678316aaf8a05eb434aee71cfdd3bc7e34ed
Džuris <juris@glaive.pro>
2023-02-04 05:07:52 +0200

    Add youthguard CSV generator

---
+++ bin/prepare_youthguard_csv.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use GlaivePro\IaafPoints\Services\Files;
+
+$dir = __DIR__.'/../data/csv/combined/';
+if (!is_dir($dir))
+	mkdir($dir, recursive: true);
+
+$files = new Files('combined');
+foreach ($files->index() as $name => $input) {
+	$output = fopen($dir.$name.'.csv', 'w');
+
+	fputcsv($output, [
+		'Gender',
+		'Age group',
+		'Discipline',
+		'Result shift',
+		'Conversion factor',
+		'Exponent',
+	]);
+
+	$data = include $input;
+	foreach ($data as $gender => $ageGroups)
+		foreach ($ageGroups as $ageGroup => $disciplines)
+			foreach ($disciplines as $discipline => $coefs)
+				fputcsv($output, [
+					$gender,
+					$ageGroup,
+					$discipline,
+					$coefs['resultShift'],
+					$coefs['conversionFactor'],
+					$coefs['exponent'],
+				]);
+}
