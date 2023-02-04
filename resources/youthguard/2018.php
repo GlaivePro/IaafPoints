d788230523f7cca2571c936642d77ec04eb0c521
Džuris <juris@glaive.pro>
2023-02-04 05:07:59 +0200

    Fix fake types

--- resources/youthguard/2018.php
+++ resources/youthguard/2018.php
@@ -4,9 +4,9 @@
  * @param array $constants
  *   Array containing coefficients to calculate points.
  *
- *   @type float $constants['edition']['gender']['discipline']['ageGroup']['resultShift']
- *   @type float $constants['edition']['gender']['discipline']['ageGroup']['conversionFactor']
- *   @type float $constants['edition']['gender']['discipline']['ageGroup']['exponent']
+ *   @type float $constants['edition']['gender']['ageGroup']['discipline']['resultShift']
+ *   @type float $constants['edition']['gender']['ageGroup']['discipline']['conversionFactor']
+ *   @type float $constants['edition']['gender']['ageGroup']['discipline']['exponent']
  */
 return [
 	'm' => [
